2c16ea0855ee72b653fb2cf82cb9fdae9b99d4ab
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-20 12:15:14 -0700

    Added simple junit test

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,11 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloTest {
 
     @Test
     public void test(){
         Assertions.assertEquals(4, 1+3);
+    }
+
+    @Test
+    public void test2(){
+        //assert 4*3 = 12
+        assertEquals(12, 4*3);
     }
 
 //commit
