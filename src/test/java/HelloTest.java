e0ba4ae1c92e1c708703bd5da171189eee192228
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-20 12:26:52 -0700

    Added @BeforeAll @AfterAll @BeforeEach @AfterEach

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,17 +1,43 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloTest {
 
+    //Junit5 Annotations
+    //@BeforeAll @AfterAll @BeforeEach @AfterEach
+
+    @BeforeAll
+    public static void setUp(){
+        System.out.println("@BeforeAll is running");
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("@AfterAll is running");
+    }
+
+    @BeforeEach
+    public void setUpTest(){
+        System.out.println("@BeforeEach is running");
+    }
+
+    @AfterEach
+    public void tearDownTest(){
+        System.out.println("@AfterEach is running");
+    }
+
     @Test
-    public void test(){
+    public void test1(){
+        System.out.println("test 1 is running");
         Assertions.assertEquals(4, 1+3);
     }
 
     @Test
     public void test2(){
         //assert 4*3 = 12
+        System.out.println("test 2 is running");
         assertEquals(12, 4*3);
     }
 
