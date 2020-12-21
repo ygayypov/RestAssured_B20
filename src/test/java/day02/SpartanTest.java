c6c44d2fc63dba38d861e0d6051327e3c62db1db
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-21 06:37:00 -0700

    created day02 package

---
+++ src/test/java/day02/SpartanTest.java
@@ -0,0 +1,19 @@
+package day02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class SpartanTest {
+
+    @DisplayName("Testing / api/spartans endpoint")
+    @Test
+    public void testGetAllSpartan(){
+
+
+    }
+}
