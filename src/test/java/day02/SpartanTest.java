cada158078f888adf2a41b1479f286935e4d9abf
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-21 06:55:10 -0700

    get all Spartans in xml

--- src/test/java/day02/SpartanTest.java
+++ src/test/java/day02/SpartanTest.java
@@ -1,10 +1,13 @@
 package day02;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 
@@ -13,6 +16,29 @@ public class SpartanTest {
     @DisplayName("Testing / api/spartans endpoint")
     @Test
     public void testGetAllSpartan(){
+
+        //send a get request to above endpoint
+        //save the response
+        //print out the result
+        //try to assert the status code
+        //content type header
+
+        Response response = get ("http://3.86.188.174:8000/api/spartans");
+        response.prettyPrint();
+
+        assertThat(response.statusCode(), is (200));
+        assertThat(response.contentType(), is(ContentType.JSON.toString()));
+        //response.contentType() is String
+        //ContentType.JSON is ENUM for that reason we added the toString to it like ContentType.JSON.toString() to make it String
+
+
+    }
+
+
+    @DisplayName("Testing / api/spartans endpoint")
+    @Test
+    public void testGetAllSpartanXML(){
+
 
 
     }
