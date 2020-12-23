4bd5ac798c472f18d43751814f0c1395eee375e1
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-22 17:41:25 -0700

    testing get the log

--- src/test/java/day04/OpenMovieDB_Test.java
+++ src/test/java/day04/OpenMovieDB_Test.java
@@ -33,12 +33,39 @@ public class OpenMovieDB_Test {
                 .queryParam("apiKey", "5b5d0fe8")
                 .queryParam("t", "The Orville").
         when()
-                .get(). // our request URL is already complete , do not need to add anything here
+                .get().prettyPeek(). // our request URL is already complete , do not need to add anything here
         then()
                 .statusCode(is(200))
                 .contentType(ContentType.JSON)
                 .body("Title", is("The Orville"))
                 .body("Ratings[0].Source" , is("Internet Movie Database"));
+
+    }
+
+
+    @DisplayName("Getting the log of request and response")
+    @Test
+    public void testSendingRequestAndGetTheLog(){
+        given()
+                .queryParam("apiKey", "5b5d0fe8")
+                .queryParam("t", "John Wick")
+                //logging the request should be in given section
+                .log().all().
+                //log().uri().
+                //log().params().
+        when()
+                .get().
+        then()
+                //logging the response should be in then section
+                .log().all()
+                //.log().status()
+                //.log().body()
+                //.log().ifValidationFails()
+                .statusCode(is(200))
+                .body("Plot", containsString("ex-hit-man"))
+                .body("Ratings[1].Source", is("Rotten Tomatoes"));
+
+
 
     }
 
