273becdee051c7116d8fbcf95486b137e7ec32cb
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-22 20:33:52 -0700

    Posting new data with String body

--- src/test/java/day04/SpartanAddingUpdatingTest.java
+++ src/test/java/day04/SpartanAddingUpdatingTest.java
@@ -12,7 +12,7 @@ public class SpartanAddingUpdatingTest {
 
     @BeforeAll
     public static void setUp(){
-        baseURI = "http://blabla";
+        baseURI = "blabla";
         basePath = "/api" ;
 
     }
@@ -35,4 +35,37 @@ public class SpartanAddingUpdatingTest {
                 .log().all()
                 .statusCode(200);
     }
+
+    @DisplayName("Add 1 Data with Raw Json String POST / api/ spartans")
+    @Test
+    public void testAddOneData(){
+
+        /*
+        {
+        "name": "Roma",
+        "gender": "Male",
+        "phone": 1234567890
+         }
+         */
+
+        String newSpartanStr = "  {\n" +
+                               "        \"name\": \"Roma\",\n" +
+                               "        \"gender\": \"Male\",\n" +
+                               "        \"phone\": 1234567890\n" +
+                               "   }" ;
+
+        System.out.println("newSpartanStr = " + newSpartanStr);
+
+        given()
+                .log().all()
+                .auth().basic("admin", "admin")
+                .contentType(ContentType.JSON)
+                .body(newSpartanStr).
+        when()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(201) ;
+    }
+
 }
