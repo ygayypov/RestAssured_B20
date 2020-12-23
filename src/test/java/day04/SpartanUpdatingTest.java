d387c71c9862df000aa0a4383c5927a27ef0a3ed
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-23 00:18:46 -0700

    Patch Request with String body Completed

--- src/test/java/day04/SpartanUpdatingTest.java
+++ src/test/java/day04/SpartanUpdatingTest.java
@@ -17,7 +17,7 @@ public class SpartanUpdatingTest {
 
     @BeforeAll
     public static void setUp(){
-        baseURI = "http://blabla";
+        baseURI = "blabla";
         basePath = "/api" ;
 
     }
@@ -29,10 +29,56 @@ public class SpartanUpdatingTest {
 
     @DisplayName("Testing PUT/api/spartans/{id}")
     @Test
-    public void testUpdatingSingleSpartan(){
+    public void testUpdatingSingleSpartanStringBody(){
 
+        String updateStrPayload = "  {\n" +
+                                   "        \"name\": \"Fiona\",\n" +
+                                   "        \"gender\": \"Female\",\n" +
+                                   "        \"phone\": 1234567555\n" +
+                                   "   }" ;
 
+        given()
+                .log().all()
+                .auth().basic("admin","admin")
+                .contentType(ContentType.JSON)
+                .pathParam("id", 691)
+                .body(updateStrPayload).
+        when()
+                .put("/spartans/{id}").
+        then()
+                .log().all()
+                .assertThat()
+                .statusCode(is(204))
+                //This is how we can check a header exists by checking the value is not null
+                //using notNullValue() matcher
+                .header("Date", is(notNullValue()))
+                .body(emptyString());
 
     }
+
+    @DisplayName("Testing PATCH/api/spartans/{id} with String body")
+    @Test
+    public void testPartialUpdatingSingleSpartanStringBody(){
+
+
+        //update the name to Marianna
+        //{"name" : "Marianna"}
+        String patchBody = "{\"name\" : \"Marianna\"}";
+        given()
+                .auth().basic("admin","admin")
+                .log().all()
+                .contentType(ContentType.JSON)
+                .pathParam("id",637)
+                .body(patchBody).
+        when()
+                .patch("/spartans/{id}").
+        then()
+                .log().all()
+                .assertThat()
+                .statusCode(is(204))
+                .body(emptyString());
+
+    }
+
 
 }
