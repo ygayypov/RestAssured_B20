b73c5702364d609e150fdbf6583948c9b2da045e
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-23 08:03:24 -0700

    LibraryAppTestStarter login test extracting the token after the validation

--- src/test/java/day04/LibraryAppTest.java
+++ src/test/java/day04/LibraryAppTest.java
@@ -37,22 +37,34 @@ public class LibraryAppTest {
         Librarian1   password          KNPXrm3S
          */
 
-        given()
-                .log().all()
-                .contentType(ContentType.URLENC)
-                .formParam("email", "librarian69@library")
-                .formParam("password", "KNPXrm3S").
-        when()
-                .post("/login").
-        then()
-                .log().all()
-                .assertThat()
-                .statusCode(is(200))
-                .contentType(ContentType.JSON)
-                .body("token" , is(not(emptyString())))
-                .body("redirect_uri", not(emptyString()))
-                ;
+        String myToken =
+                given()
+                        .log().all()
+                        .contentType(ContentType.URLENC)
+                        .formParam("email", "librarian69@library")
+                        .formParam("password", "KNPXrm3S").
+                when()
+                        .post("/login").
+                then()
+                        .log().all()
+                        .assertThat()
+                        .statusCode(is(200))
+                        .contentType(ContentType.JSON)
+                        .body("token" , is(not(emptyString())))
+                        .body("redirect_uri", not(emptyString()))
+                    .extract()
+                         .jsonPath()
+                         .getString("token")
+                        ;
 
+        System.out.println("myToken = " + myToken);
+
+        //authentication tells the system who you are
+        //authorization according who you are things you can do
+
+
+        //How to extract some data out of response object after doing validation in then section without
+        // breaking the chain -->> use extract() method that return
 
 
     }
