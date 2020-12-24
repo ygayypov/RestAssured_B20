7539765668fd4f9038e4a71e21a54bf771f4e28b
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-23 22:55:56 -0700

    Test Method Ordering in Junit 5 by ordering annotation

--- src/test/java/day05/TestOrderingInJunit5.java
+++ src/test/java/day05/TestOrderingInJunit5.java
@@ -1,24 +1,30 @@
 package day05;
-
+//by default the test is running on alphabetical order
+//or the test method name
 import org.junit.jupiter.api.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestOrderingInJunit5 {
 
+    @Order(3)
     @Test
     public void testA(){
         System.out.println("running test A");
     }
 
+    @Order(1)
     @Test
     public void testC(){
         System.out.println("running test C");
     }
 
+    @Order(4)
     @Test
     public void testD(){
         System.out.println("running test D");
     }
 
+    @Order(2)
     @Test
     public void testB(){
         System.out.println("running test B");
