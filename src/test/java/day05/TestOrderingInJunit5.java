fda501574e1cfdfbf3c363630b48e2f90caff9bf
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-23 22:49:08 -0700

    Test Method Ordering

---
+++ src/test/java/day05/TestOrderingInJunit5.java
@@ -0,0 +1,27 @@
+package day05;
+
+import org.junit.jupiter.api.*;
+
+public class TestOrderingInJunit5 {
+
+    @Test
+    public void testA(){
+        System.out.println("running test A");
+    }
+
+    @Test
+    public void testC(){
+        System.out.println("running test C");
+    }
+
+    @Test
+    public void testD(){
+        System.out.println("running test D");
+    }
+
+    @Test
+    public void testB(){
+        System.out.println("running test B");
+    }
+
+}
