97541b72ae9e3f5cdc59cd7cc8d676df8ff0cb3d
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-25 11:58:25 -0700

    Added getRandomSpartanPOJO_Payload utility method for creating spartan object with random field value

--- src/test/java/utility/SpartanUtil.java
+++ src/test/java/utility/SpartanUtil.java
@@ -1,6 +1,7 @@
 package utility;
 
 import com.github.javafaker.Faker;
+import pojo.Spartan;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -20,7 +21,31 @@ public class SpartanUtil {
 
     }
 
+    //below main method is to check the above return payloadMap
+    /*
     public static void main(String[] args) {
         System.out.println(getRandomSpartanRequestPayload());
     }
+    */
+
+    public static Spartan getRandomSpartanPOJO_Payload(){
+
+        Faker faker = new Faker();
+
+        Spartan randomSpartan = new Spartan();
+        randomSpartan.setName(faker.name().firstName());
+        randomSpartan.setGender(faker.demographic().sex());
+        randomSpartan.setPhone(faker.number().numberBetween(5000000000L, 9999999999L));
+
+        return randomSpartan;
+    }
+
+    //below main method is to check the above return randomSpartan
+    /*
+    public static void main(String[] args) {
+        System.out.println(getRandomSpartanPOJO_Payload());
+    }
+    */
+
+
 }
