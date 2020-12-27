1dde3c9f504a530d17cf549d0f0509c01c380ff7
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-27 11:16:57 -0700

    Partial One Spartan Updated

--- src/test/java/day07/PatchOneSpartanTest.java
+++ src/test/java/day07/PatchOneSpartanTest.java
@@ -6,6 +6,9 @@ import pojo.Spartan;
 import utility.ConfigurationReader;
 import utility.SpartanUtil;
 
+import java.util.LinkedHashMap;
+import java.util.*;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -28,7 +31,50 @@ public class PatchOneSpartanTest {//partial update
     @Test
     public void testPath1DataPartialUpdate(){
 
+        //we just wanted to update the name and phone number
 
+        Map<String , Object> patchBodyMap = new LinkedHashMap<>();
+        patchBodyMap.put("name", "Bolajonim");
+        patchBodyMap.put("phone", 1233211236L);
+
+        given()
+                .auth().basic("admin", "admin")
+                .log().all()
+                .pathParam("id" , 279)
+                .contentType(ContentType.JSON)
+                .body(patchBodyMap).
+        when()
+                .patch("/spartans/{id}").
+        then()
+                .log().all()
+                .statusCode(204);
+
+    }
+
+
+    @DisplayName("Patching 1 data with POJO")
+    @Test
+    public void testPath1DataPartialUpdateWithPOJO(){
+
+        //we just wanted to update the name and phone number
+
+        Spartan sp = new Spartan();
+        sp.setName("BAYE");
+        sp.setPhone(3456984562L);
+
+        //Map is a better option with minimal effort
+        //POJO class need some handling to ignore empty values when being serialized
+        given()
+                .auth().basic("admin", "admin")
+                .log().all()
+                .pathParam("id" , 280)
+                .contentType(ContentType.JSON)
+                .body(sp).
+        when()
+                .patch("/spartans/{id}").
+        then()
+                .log().all()
+                .statusCode(204);
 
     }
 
