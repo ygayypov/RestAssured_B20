a979d1f2859277613c015e4ae19fb4ccac3e766c
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-12-27 22:57:18 -0700

    HR ORDS class created

---
+++ src/test/java/day07/HR_ORDS_Test.java
@@ -0,0 +1,51 @@
+package day07;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.*;
+import pojo.BookCategory;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.*;
+
+public class HR_ORDS_Test {
+
+    //http://54.90.101.103:1000/ords/hr/regions/3
+    @BeforeAll
+    @Test
+    public static void setUp(){
+        baseURI = "http://54.90.101.103:1000";
+        basePath = "/ords/hr";
+
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+
+    @DisplayName("Testing /regions/{region_id}")
+    @Test
+    public void testThirdRegionIsAsia(){
+
+        given()
+                .pathParam("region_id", 3)
+                .log().all().
+        when()
+                .get("/regions/{region_id}").
+        then()
+                .log().all()
+                .assertThat()
+                .statusCode(is(200))
+                .contentType(ContentType.JSON)
+                .body("region_name" , is("Asia"))
+                ;
+
+    }
+
+}
