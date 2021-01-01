ba2f4547df19aaa3228f35d5d595f129821050d4
ygayypov <68933447+ygayypov@users.noreply.github.com>
2021-01-01 08:43:35 -0700

    ORDS_API_DB_PracticeTest class created

---
+++ src/test/java/day09/ORDS_API_DB_PracticeTest.java
@@ -0,0 +1,16 @@
+package day09;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import testbase.HR_ORDS_TestBase;
+
+public class ORDS_API_DB_PracticeTest extends HR_ORDS_TestBase { //inheritance
+
+    @DisplayName("GET /countries /{country_id} Compare Result with DB")
+    @Test
+    public void testResponseMatchDatabaseData(){
+
+
+    }
+
+}
