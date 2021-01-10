ee1e24416a919165bbee2848faedbf1dac1afcbf
ygayypov <68933447+ygayypov@users.noreply.github.com>
2021-01-10 09:01:05 -0700

    Parameterized Test for zipcode

--- src/test/java/day09/JUnit5_ParameterizedTest.java
+++ src/test/java/day09/JUnit5_ParameterizedTest.java
@@ -1,8 +1,13 @@
 package day09;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class JUnit5_ParameterizedTest {
 
@@ -17,4 +22,30 @@ public class JUnit5_ParameterizedTest {
     }
 
     //using CSV(comma separated values) file as source for parametrized
+    @ParameterizedTest
+    @CsvFileSource(resources = "/zipcode.csv", numLinesToSkip = 1)//we use numLinesToSkip = 1 to skip the header
+    public void test2 (String zip){
+
+        System.out.println("zip = " + zip);
+
+        //sending request to zipcode endpoint here
+        //api.zippopotam.us/us/87112
+        //api.zippopotam.us/us/{zipcode}
+        //baseurl: api.zippopotam.us
+        //endpoint is : /us/{zipcode}
+
+        given()
+                .log().uri()
+                .baseUri("https://api.zippopotam.us")
+                .pathParam("zipcode", zip).
+        when()
+                .get("/us/{zipcode}").
+        then()
+                .statusCode(200);
+
+        //restAssured test you did above
+
+    }
+
+
 }
